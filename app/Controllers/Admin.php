1043d7f46d5806f7be6d00acc21159d3a56482e8
Diandara TResya Haniva <diandaraa@gmail.com>
2023-01-01 21:21:16 +0700

    debugging

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -15,6 +15,7 @@ class Admin extends BaseController
   protected $CustomModel;
   protected $PemanduModel;
   protected $WisatawanModel;
+
   public function __construct()
   {
     $this->AdminModel = new AdminModel();
@@ -23,8 +24,10 @@ class Admin extends BaseController
     $this->PemanduModel = new PemanduModel();
     $this->WisatawanModel = new WisatawanModel();
   }
+
   public function index()
   {
+
     $data = [
       'admin' => $this->AdminModel->where('id', session()->get('admin_id'))->first(),
       'dataTransaksi' => $this->CustomModel->transaksiTerbaru(),
@@ -34,21 +37,28 @@ class Admin extends BaseController
     ];
     return view('admin/index', $data);
   }
+
   public function indexWisatawan()
   {
     $data = [
       'dataWisatawan' => $this->WisatawanModel->findAll(),
       'admin' => $this->AdminModel->where('id', session()->get('admin_id'))->first()
     ];
+
+
+
     return view('admin/wisatawan/index', $data);
   }
+
   public function tambahWisatawan()
   {
     $data = [
       'admin' => $this->AdminModel->where('id', session()->get('admin_id'))->first(),
+
     ];
     return view('/admin/wisatawan/tambah', $data);
   }
+
   public function prosesTambahWisatawan()
   {
     $rules = $this->validate([
@@ -77,17 +87,22 @@ class Admin extends BaseController
         ]
       ],
     ]);
+
     if (!$rules) {
       session()->setFlashdata('error', $this->validator->listErrors());
       return redirect()->back()->withInput();
     }
+
     $password = $this->request->getVar('password');
     $passwordVerif = $this->request->getVar('passwordVerif');
+
     if ($password != $passwordVerif) {
       session()->setFlashdata('error', 'Password konfirmasi tidak sama dengan password');
       return redirect()->back()->withInput();
     }
+
     $passwordHas = password_hash($password, PASSWORD_BCRYPT);
+
     $data = [
       'nama' => $this->request->getVar('nama'),
       'email' => $this->request->getVar('email'),
@@ -95,18 +110,22 @@ class Admin extends BaseController
       'password' => $passwordHas,
       'image' => 'default.png'
     ];
+
     $this->WisatawanModel->insert($data);
     session()->setFlashdata('success', 'Data berhasil ditambahkan');
     return redirect()->to('/admin/wisatawan');
   }
+
   public function editWisatawan($id = null)
   {
     $data = [
       'wisatawan' => $this->WisatawanModel->where('id', $id)->first(),
       'admin' => $this->AdminModel->where('id', session()->get('admin_id'))->first(),
     ];
+
     return view('admin/wisatawan/edit', $data);
   }
+
   public function prosesEditWisatawan()
   {
     $rules = $this->validate([
@@ -129,15 +148,25 @@ class Admin extends BaseController
         ]
       ],
     ]);
+
     if (!$rules) {
       session()->setFlashdata('error', $this->validator->listErrors());
       return redirect()->back()->withInput();
     }
+
+
+
     $id = $this->request->getVar('id');
+
     $password = $this->request->getVar('password');
     $passwordVerif = $this->request->getVar('passwordVerif');
+
     date_default_timezone_set('Asia/Jakarta');
     $image = $_FILES['file']['name'];
+
+
+
+
     if (!$image) {
       if (!$password) {
         $data = [
@@ -145,25 +174,31 @@ class Admin extends BaseController
           'email' => $this->request->getVar('email'),
           'telepon' => $this->request->getVar('telepon'),
         ];
+
         $this->WisatawanModel->update($id, $data);
         session()->setFlashdata('success', 'Data berhasil di perbarui');
         return redirect()->to('/admin/wisatawan');
       }
+
       if ($password != $passwordVerif) {
         session()->setFlashdata('error', 'Password konfirmasi tidak sama dengan password');
         return redirect()->back()->withInput();
       }
+
       $passwordHas = password_hash($password, PASSWORD_BCRYPT);
+
       $data = [
         'nama' => $this->request->getVar('nama'),
         'email' => $this->request->getVar('email'),
         'telepon' => $this->request->getVar('telepon'),
         'password' => $passwordHas,
       ];
+
       $this->WisatawanModel->update($id, $data);
       session()->setFlashdata('success', 'Data berhasil di perbarui');
       return redirect()->to('/admin/wisatawan');
     }
+
     $image = $this->request->getFile('file');
     $rulesImage = $this->validate([
       'file' => [
@@ -171,38 +206,49 @@ class Admin extends BaseController
         'mime_in[file,image/png,image/jpg,image/jpeg]',
       ],
     ]);
+
     if (!$rulesImage) {
       session()->setFlashdata('error', $this->validator->listErrors());
       return redirect()->back();
     }
+
     if (!$password) {
       $file = $image->getName();
       $info = pathinfo($file);
+
       $file_name =  $info['filename'];
       $slug = strtolower(preg_replace('/[^A-Za-z0-9-]+/', '-', $file_name));
       $newNameImage = $slug . '_' . date('Y-m-d') . '_' . date('H-i-s') . '.' . $image->getClientExtension();
       $image->move('assets/img/wisatawan/', $newNameImage);
+
       $data = [
         'nama' => $this->request->getVar('nama'),
         'email' => $this->request->getVar('email'),
         'telepon' => $this->request->getVar('telepon'),
         'image' => $newNameImage
       ];
+
       $this->WisatawanModel->update($id, $data);
       session()->setFlashdata('success', 'Data berhasil di perbarui');
       return redirect()->to('/admin/wisatawan');
     }
+
+
     if ($password != $passwordVerif) {
       session()->setFlashdata('error', 'Password konfirmasi tidak sama dengan password');
       return redirect()->back()->withInput();
     }
+
     $passwordHas = password_hash($password, PASSWORD_BCRYPT);
+
     $file = $image->getName();
     $info = pathinfo($file);
+
     $file_name =  $info['filename'];
     $slug = strtolower(preg_replace('/[^A-Za-z0-9-]+/', '-', $file_name));
     $newNameImage = $slug . '_' . date('Y-m-d') . '_' . date('H-i-s') . '.' . $image->getClientExtension();
     $image->move('assets/img/wisatawan/', $newNameImage);
+
     $data = [
       'nama' => $this->request->getVar('nama'),
       'email' => $this->request->getVar('email'),
@@ -210,18 +256,23 @@ class Admin extends BaseController
       'password' => $passwordHas,
       'image' => $newNameImage
     ];
+
     $this->WisatawanModel->update($id, $data);
     session()->setFlashdata('success', 'Data berhasil di perbarui');
     return redirect()->to('/admin/wisatawan');
   }
+
   public function indexPemandu()
   {
     $data = [
       'dataPemandu' => $this->PemanduModel->findAll(),
       'admin' => $this->AdminModel->where('id', session()->get('admin_id'))->first(),
+
     ];
+
     return view('admin/pemandu/index', $data);
   }
+
   public function tambahPemandu()
   {
     $data = [
@@ -229,6 +280,7 @@ class Admin extends BaseController
     ];
     return view('/admin/pemandu/tambah', $data);
   }
+
   public function prosesTambahPemandu()
   {
     $rules = $this->validate([
@@ -275,17 +327,22 @@ class Admin extends BaseController
         ]
       ],
     ]);
+
     if (!$rules) {
       session()->setFlashdata('error', $this->validator->listErrors());
       return redirect()->back()->withInput();
     }
+
     $password = $this->request->getVar('password');
     $passwordVerif = $this->request->getVar('passwordVerif');
+
     if ($password != $passwordVerif) {
       session()->setFlashdata('error', 'Password konfirmasi tidak sama dengan password');
       return redirect()->back()->withInput();
     }
+
     $passwordHas = password_hash($password, PASSWORD_BCRYPT);
+
     $data = [
       'noKta' => $this->request->getVar('noKta'),
       'nama' => $this->request->getVar('nama'),
@@ -296,18 +353,23 @@ class Admin extends BaseController
       'password' => $passwordHas,
       'image' => 'default.png'
     ];
+
     $this->PemanduModel->insert($data);
     session()->setFlashdata('success', 'Data berhasil ditambahkan');
     return redirect()->to('/admin/pemandu');
   }
+
   public function editPemandu($id = null)
   {
     $data = [
       'pemandu' => $this->PemanduModel->where('id', $id)->first(),
       'admin' => $this->AdminModel->where('id', session()->get('admin_id'))->first(),
+
     ];
+
     return view('admin/pemandu/edit', $data);
   }
+
   public function prosesEditPemandu()
   {
     $rules = $this->validate([
@@ -359,11 +421,20 @@ class Admin extends BaseController
       session()->setFlashdata('error', $this->validator->listErrors());
       return redirect()->back()->withInput();
     }
+
+
+
     $id = $this->request->getVar('id');
+
     $password = $this->request->getVar('password');
     $passwordVerif = $this->request->getVar('passwordVerif');
+
     date_default_timezone_set('Asia/Jakarta');
     $image = $_FILES['file']['name'];
+
+
+
+
     if (!$image) {
       if (!$password) {
         $data = [
@@ -375,15 +446,19 @@ class Admin extends BaseController
           'jenis' => $this->request->getVar('jenis'),
           'ringkasan' => $this->request->getVar('ringkasan'),
         ];
+
         $this->PemanduModel->update($id, $data);
         session()->setFlashdata('success', 'Data berhasil di perbarui');
         return redirect()->to('/admin/pemandu');
       }
+
       if ($password != $passwordVerif) {
         session()->setFlashdata('error', 'Password konfirmasi tidak sama dengan password');
         return redirect()->back()->withInput();
       }
+
       $passwordHas = password_hash($password, PASSWORD_BCRYPT);
+
       $data = [
         'noKta' => $this->request->getVar('noKta'),
         'nama' => $this->request->getVar('nama'),
@@ -394,10 +469,12 @@ class Admin extends BaseController
         'ringkasan' => $this->request->getVar('ringkasan'),
         'password' => $passwordHas,
       ];
+
       $this->PemanduModel->update($id, $data);
       session()->setFlashdata('success', 'Data berhasil di perbarui');
       return redirect()->to('/admin/pemandu');
     }
+
     $image = $this->request->getFile('file');
     $rulesImage = $this->validate([
       'file' => [
@@ -405,17 +482,21 @@ class Admin extends BaseController
         'mime_in[file,image/png,image/jpg,image/jpeg]',
       ],
     ]);
+
     if (!$rulesImage) {
       session()->setFlashdata('error', $this->validator->listErrors());
       return redirect()->back();
     }
+
     if (!$password) {
       $file = $image->getName();
       $info = pathinfo($file);
+
       $file_name =  $info['filename'];
       $slug = strtolower(preg_replace('/[^A-Za-z0-9-]+/', '-', $file_name));
       $newNameImage = $slug . '_' . date('Y-m-d') . '_' . date('H-i-s') . '.' . $image->getClientExtension();
       $image->move('assets/img/pemandu/', $newNameImage);
+
       $data = [
         'noKta' => $this->request->getVar('noKta'),
         'nama' => $this->request->getVar('nama'),
@@ -426,21 +507,28 @@ class Admin extends BaseController
         'ringkasan' => $this->request->getVar('ringkasan'),
         'image' => $newNameImage
       ];
+
       $this->PemanduModel->update($id, $data);
       session()->setFlashdata('success', 'Data berhasil di perbarui');
       return redirect()->to('/admin/pemandu');
     }
+
+
     if ($password != $passwordVerif) {
       session()->setFlashdata('error', 'Password konfirmasi tidak sama dengan password');
       return redirect()->back()->withInput();
     }
+
     $passwordHas = password_hash($password, PASSWORD_BCRYPT);
+
     $file = $image->getName();
     $info = pathinfo($file);
+
     $file_name =  $info['filename'];
     $slug = strtolower(preg_replace('/[^A-Za-z0-9-]+/', '-', $file_name));
     $newNameImage = $slug . '_' . date('Y-m-d') . '_' . date('H-i-s') . '.' . $image->getClientExtension();
     $image->move('assets/img/pemandu/', $newNameImage);
+
     $data = [
       'noKta' => $this->request->getVar('noKta'),
       'nama' => $this->request->getVar('nama'),
@@ -452,16 +540,19 @@ class Admin extends BaseController
       'password' => $passwordHas,
       'image' => $newNameImage
     ];
+
     $this->PemanduModel->update($id, $data);
     session()->setFlashdata('success', 'Data berhasil di perbarui');
     return redirect()->to('/admin/pemandu');
   }
+
   public function deleteWisatawan($id = null)
   {
     $this->WisatawanModel->delete($id);
     session()->setFlashdata('success', 'Data berhasil dihapus');
     return redirect()->to('/admin/wisatawan');
   }
+
   public function deletePemandu($id = null)
   {
     $this->PemanduModel->delete($id);
